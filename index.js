33632361a01c505357bb6f73a3de0ed1eee972fc
David Floyd <davidfloyd91@gmail.com>
2018-10-27 23:03:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   return Object.assign(object, {[key]: value});
 }
 function deleteFromObjectByKey(object, key) {
-  return delete object.key;
+  return delete object[key];
 }
 function destructivelyDeleteFromObjectByKey(object, key) {
-  delete object.key;
+  delete object[key];
   return object;
 }
